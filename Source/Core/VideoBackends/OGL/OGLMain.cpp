6228ee38de06edcdf18ac574413e745572d29f2b
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2022-03-20 13:16:54 +0100

    geometry shader
    not needed for now

--- Source/Core/VideoBackends/OGL/OGLMain.cpp
+++ Source/Core/VideoBackends/OGL/OGLMain.cpp
@@ -78,7 +78,7 @@ void VideoBackend::InitBackendInfo()
   g_Config.backend_info.bUsesLowerLeftOrigin = true;
   g_Config.backend_info.bSupportsExclusiveFullscreen = false;
   g_Config.backend_info.bSupportsOversizedViewports = true;
-  g_Config.backend_info.bSupportsGeometryShaders = true;
+  g_Config.backend_info.bSupportsGeometryShaders = false; // true;
   g_Config.backend_info.bSupportsComputeShaders = false;
   g_Config.backend_info.bSupports3DVision = false;
   g_Config.backend_info.bSupportsPostProcessing = true;
